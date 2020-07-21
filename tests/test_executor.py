367d6fd0e5497f860d1231db84b58a8d459935b9
Paul Gessinger <hello@paulgessinger.com>
2020-07-21 13:47:22 +0200

    test(executor): improve coverage

---
+++ tests/test_executor.py
@@ -0,0 +1,27 @@
+from kong.executor import SerialExecutor
+
+def test_SerialExecutor():
+    ex = SerialExecutor()
+
+    results = []
+
+    func = lambda n: results.append(n)
+
+    f = ex.submit(func, 1)
+    assert results == [1]
+    assert f.exception() is None
+
+    f = ex.submit(func, 42)
+    assert results == [1, 42]
+    assert f.exception() is None
+
+    def error():
+        raise ValueError()
+
+    f = ex.submit(error)
+    assert isinstance(f.exception(), ValueError)
+
+    ret = lambda x: x*x
+
+    f = ex.submit(ret, 4)
+    assert f.result() == 16
