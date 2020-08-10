a048737ea69f2bfd91d6826b3248e080d4b11d0d
Paul Gessinger <hello@paulgessinger.com>
2020-08-10 19:48:35 +0200

    fix(drivers): Do not submit jobs in transaction
    This might seem more efficient, but if one submission fails for whatever
    reason, all the status updates and job id collection is rolled back,
    leaving the jobs useless.

--- src/kong/drivers/batch_driver_base.py
+++ src/kong/drivers/batch_driver_base.py
@@ -106,19 +106,11 @@ class BatchDriverBase(DriverBase):
     def bulk_submit(self, jobs: Iterable["Job"]) -> None:
         now = datetime.datetime.utcnow()
 
-        def sub() -> Iterable[Job]:
-            for job in jobs:
-                assert job.driver == self.__class__, "Not valid for different driver"
-                self.submit(job, save=False)
-                job.updated_at = now
-                yield job
-
-        with database.atomic():
-            Job.bulk_update(
-                sub(),
-                fields=[Job.batch_job_id, Job.status, Job.updated_at],
-                batch_size=self.batch_size,
-            )
+        for job in jobs:
+            assert job.driver == self.__class__, "Not valid for different driver"
+            self.submit(job, save=False)
+            job.updated_at = now
+            job.save()
 
     @checked_job  # type: ignore
     @contextmanager  # type: ignore
